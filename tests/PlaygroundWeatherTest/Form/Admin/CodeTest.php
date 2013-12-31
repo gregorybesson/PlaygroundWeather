516e913b323a64c6a84093bd01bdab3caa9c1aa8
stephaniefaureadfab <stephanie.faure@adfab.fr>
2013-12-31 12:31:16 +0100

    added ViewHelper temperature + not showing on mpa towns for which we do not have data + check permission on accessing WWO before trying to parse

---
+++ tests/PlaygroundWeatherTest/Form/Admin/CodeTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace PlaygroundWeatherTest\Form\Admin;
+
+use PlaygroundWeatherTest\Bootstrap;
+use PlaygroundWeather\Form\Admin\Code;
+
+class CodeTest extends \PHPUnit_Framework_TestCase
+{
+    protected $sm;
+
+    protected $translator;
+
+    protected $form;
+
+    protected $codesData;
+
+    public function setUp()
+    {
+        $this->sm = Bootstrap::getServiceManager();
+
+        $this->getForm();
+        $this->codesData = array(
+            'codes' => array(),
+        );
+
+        parent::setUp();
+    }
+
+    public function testHandle7Codes()
+    {
+        $this->form->get('codes')->setCount(7)->prepareFieldset();
+        $this->form->setData($this->codesData);
+        var_dump($this->form->isValid());
+        $data = $this->form->getData();
+        $this->assertCount(7, $data['codes']);
+    }
+
+    public function getForm()
+    {
+        if (null === $this->form) {
+            $this->form = $this->sm->get('playgroundweather_code_form');
+        }
+        return $this->form;
+    }
+}
